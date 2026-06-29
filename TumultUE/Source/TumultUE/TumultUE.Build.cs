5d4da1d65d9729c01b3a69e1af83163f86e45e5c
KirkDiggler <kirklgilmore@gmail.com>
2026-06-28 17:07:09 -0700

    fix: make Tumult include path configurable

--- TumultUE/Source/TumultUE/TumultUE.Build.cs
+++ TumultUE/Source/TumultUE/TumultUE.Build.cs
@@ -1,5 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System;
 using System.IO;
 using UnrealBuildTool;
 
@@ -12,14 +13,27 @@ public class TumultUE : ModuleRules
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateIncludePaths.Add(Path.GetFullPath(Path.Combine(
-			ModuleDirectory,
-			"..",
-			"..",
-			"..",
-			"..",
-			"tumult",
-			"include")));
+		string TumultRoot = Environment.GetEnvironmentVariable("TUMULT_ROOT");
+		if (string.IsNullOrWhiteSpace(TumultRoot))
+		{
+			TumultRoot = Path.GetFullPath(Path.Combine(
+				ModuleDirectory,
+				"..",
+				"..",
+				"..",
+				"..",
+				"tumult"));
+		}
+
+		string TumultIncludePath = Path.Combine(TumultRoot, "include");
+		if (!Directory.Exists(TumultIncludePath))
+		{
+			throw new BuildException(
+				"Tumult headers not found at '{0}'. Place tumult beside tumult-ue or set TUMULT_ROOT to the Tumult repository root.",
+				TumultIncludePath);
+		}
+
+		PrivateIncludePaths.Add(TumultIncludePath);
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
